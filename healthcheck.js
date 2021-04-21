f2cb431af59f4bea6837acb9dcd15514026d5273
Dmitrii Belozyorov <dmitrii.belozyorov@gmail.com>
2021-04-21 22:45:19 +0300

    Fix healthcheck

--- healthcheck.js
+++ healthcheck.js
@@ -1,7 +1,9 @@
 var http = require("http");
+const os = require('os');
+const host = process.env.HOST || os.hostname()
 
 var options = {
-    host: "localhost",
+    host: host,
     port: process.env.REGISTAR_HOST ? process.env.PORT : process.env.REGISTAR_PORT,
     timeout: 5000,
     path: "/healthcheck",
@@ -18,7 +20,7 @@ var request = http.request(options, (res) => {
 });
 
 request.on('error', function(err){
-    console.log('ERROR');
+    console.log(err);
     process.exit(1);
 });
 
